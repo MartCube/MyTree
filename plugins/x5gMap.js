82266d8ca0aa405c1a4a22e6915f1c8d4558c50b
mart cube <martingjorceski@gmail.com>
2020-09-02 22:17:28 +0300

    env

--- plugins/x5gMap.js
+++ plugins/x5gMap.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import x5GMaps from 'x5-gmaps'
 
-Vue.use(x5GMaps, process.env.gmapKey)
+// Vue.use(x5GMaps, process.env.gmapKey)
 
-// export default ({ $config: { gmapKey } }) => {
-// 	Vue.use(x5GMaps, gmapKey)
-// }
+export default ({ $config: { gmapKey } }) => {
+	Vue.use(x5GMaps, gmapKey)
+}
